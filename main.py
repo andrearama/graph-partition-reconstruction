2f4103af48a268007e127ba267544069da97333d
andrama <andrea_ramazzina@hotmail.it>
2018-03-05 17:39:12 +0100

    Add graph clustering coeff versus KL

--- main.py
+++ main.py
@@ -26,11 +26,26 @@ shapes = ['^','o'] #One for each community detection algorithm
 modes = ['louvain','label_prop'] 
 colors = ['b','r','g'] #One for each graph
 
+
 for i in range(0,len(shapes)):
     shape = shapes[i]
     mode = modes[i]
     
     for count, G in enumerate(graphs):
         kl_o2p,kl_p2o = partition_and_divergence(G,mode)
+        clustering_coefficient = nx.average_clustering(G)
         setting = colors[count]+shape
+        
+        plt.subplot(1, 2, 1)
         plt.plot(kl_o2p,kl_p2o,setting,mfc='none')
+        
+        plt.subplot(1, 2, 2)
+        plt.plot(clustering_coefficient,kl_p2o,setting,mfc='none')
+
+plt.subplot(1, 2, 1)        
+plt.xlabel('KL[P0;P*]')
+plt.ylabel('KL[P*;P0]')
+plt.subplot(1, 2, 2)
+plt.xlabel('clustering coefficient')
+plt.ylabel('KL[P*;P0]')
+plt.show()
