7bdaccb204367352b208f0dd1619c686029cbe2a
andrama <andrea_ramazzina@hotmail.it>
2018-03-05 18:09:36 +0100

    Added few more graph-comparison options

--- main.py
+++ main.py
@@ -27,20 +27,30 @@ modes = ['louvain','label_prop']
 colors = ['b','r','g'] #One for each graph
 
 
-for i in range(0,len(shapes)):
-    shape = shapes[i]
-    mode = modes[i]
+for count, G in enumerate(graphs):
     
-    for count, G in enumerate(graphs):
+    for i in range(0,len(shapes)):
+        shape = shapes[i]
+        mode = modes[i]
+        setting = colors[count]+shape
+        
         kl_o2p,kl_p2o = partition_and_divergence(G,mode)
         clustering_coefficient = nx.average_clustering(G)
-        setting = colors[count]+shape
+        density_coefficient = nx.density(G)        
         
         plt.subplot(1, 2, 1)
         plt.plot(kl_o2p,kl_p2o,setting,mfc='none')
         
-        plt.subplot(1, 2, 2)
+        plt.subplot(1, 2, 2) #looks like as the clustering increases the difference in results decreases.
         plt.plot(clustering_coefficient,kl_p2o,setting,mfc='none')
+ 
+#        plt.subplot(1, 2, 2) # Similar to the other one
+#        plt.plot(len(G),kl_p2o,setting,mfc='none')        
+        
+#        plt.subplot(1, 2, 2)  #looks like as the density decreases the difference in results decreases.
+#        plt.plot(density_coefficient,kl_p2o,setting,mfc='none')
+        
+#        
 
 plt.subplot(1, 2, 1)        
 plt.xlabel('KL[P0;P*]')
