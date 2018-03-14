3fa07dcc2d54c09c3d20cb39368719ea0776d540
Lukas Mericle <lukas.mericle@gmail.com>
2018-03-14 20:16:42 +0100

    make plotting prettier

--- plot_imgs.py
+++ plot_imgs.py
@@ -7,13 +7,13 @@ with open("results.pickle", "rb") as f:
 names = ['random', 'smallworld', 'prefgrowh', 'rome', 'swuspg', 'jazz']
 colors = ['red', 'blue', 'green', 'orange', 'brown', 'purple']
 
-shapes = ['x', '', '*'] #One for each community detection algorithm
-modes = ['louvain', 'mapeq', 'label_prop']
+shapes = ['x', '*', ''] #One for each community detection algorithm
+modes = ['louvain', 'label_prop', 'mapeq']
 
-fig, ax = plt.subplots(2, 2)
+fig, ax = plt.subplots(2, 2, figsize=(14,10))
 
 #ax[0,0].set_xscale('log')
-ax[0,0].set_yscale('log')
+#ax[0,0].set_yscale('log')
 
 ax[0,1].set_xscale('log')
 ax[0,1].set_yscale('log')
@@ -21,7 +21,7 @@ ax[0,1].set_yscale('log')
 #ax[1,0].set_xscale('log')
 ax[1,0].set_yscale('log')
 
-#ax[1,1].set_xscale('log')
+ax[1,1].set_xscale('log')
 ax[1,1].set_yscale('log')
 
 for name,color in zip(names,colors):
@@ -44,10 +44,54 @@ for name,color in zip(names,colors):
 
             if mode=='mapeq':
                 shape = (i+3, 0, 0) # for enumerating the different partitions
-            ax[0,0].plot(o2p[i]/nnd, p2o[i]/nnd, marker=shape, mec=color, mfc='none') # p2o v o2p
-            ax[0,1].plot(ncl[i]/nnd, p2o[i]/nnd, marker=shape, mec=color, mfc='none') # p2o v n_clusters
-            ax[1,0].plot(cvg[i], p2o[i]/nnd, marker=shape, mec=color, mfc='none') # p2o v 'coverage'
-            ax[1,1].plot(dnc, p2o[i]/nnd, marker=shape, mec=color, mfc='none') # p2o v 'density'
+            ax[0,0].plot(o2p[i]/nnd if o2p[i]>0 else 0, p2o[i]/nnd, marker=shape, ms=10, mew=1.5, mec=color, mfc='none') # p2o v o2p
+            ax[0,1].plot(ncl[i]/nnd, p2o[i]/nnd, marker=shape, ms=10, mew=1.5, mec=color, mfc='none') # p2o v n_clusters
+            ax[1,0].plot(cvg[i], p2o[i]/nnd, marker=shape, ms=10, mew=1.5, mec=color, mfc='none') # p2o v 'coverage'
+            ax[1,1].plot(dnc, p2o[i]/nnd, marker=shape, ms=10, mew=1.5, mec=color, mfc='none') # p2o v 'density'
 
+
+fs = 15
+ax[0,0].set_xlabel("K[P0;P*] / N", fontsize=fs)
+ax[0,0].set_ylabel("K[P*;P0] / N", fontsize=fs)
+
+ax[0,1].set_xlabel("Num clusters / N", fontsize=fs)
+ax[0,1].set_ylabel("K[P*;P0] / N", fontsize=fs)
+
+ax[1,0].set_xlabel("Coverage", fontsize=fs)
+ax[1,0].set_ylabel("K[P*;P0] / N", fontsize=fs)
+
+ax[1,1].set_xlabel("Density coeff.", fontsize=fs)
+ax[1,1].set_ylabel("K[P*;P0] / N", fontsize=fs)
+
+import matplotlib.lines as mlines
+
+patches = []
+labels = []
+p = mlines.Line2D([0], [0], marker='x', mec='k', mfc='none', ms=10, ls='', label="Louvain")
+l = "Louvain"
+labels.append(l)
+patches.append(p)
+p = mlines.Line2D([0], [0], marker='*', mec='k', mfc='none', ms=10, ls='', label="Label Prop.")
+l = "Label Prop."
+labels.append(l)
+patches.append(p)
+for i in range(6):
+    p = mlines.Line2D([0], [0], marker=(i+3, 0, 0), mec='k', mfc='none', ms=10, ls='', label="MapEq, Level {:d}".format(i))
+    l = "MapEq, Level {:d}".format(i)
+    labels.append(l)
+    patches.append(p)
+
+#pc = mcollns.PatchCollection(patches)
+ax[0,0].legend(handles=patches)
+
+import matplotlib.patches as mpatches
+
+patches = []
+for name,color in zip(names,colors):
+    p = mpatches.Patch(color=color, label=name)
+    patches.append(p)
+ax[1,1].legend(handles=patches)
+
+plt.tight_layout()
 plt.savefig("results.png")
 plt.show()
